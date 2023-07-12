73fde807e11dbe54c897118d6e404722de3848e0
the-remeko <remekocs+remeko@gmail.com>
2023-07-11 19:10:15 -0500

    Added upgrade panel, upgrades use reflection and attributes for now

--- code/Game.Commands.cs
+++ code/Game.Commands.cs
@@ -9,14 +9,32 @@ namespace DeadLines;
 
 public partial class DeadLines
 {
-	[ConCmd.Server("kill")]
+	[ConCmd.Server( "kill" )]
 	public static void KillCmd()
 	{
 		var pawn = ConsoleSystem.Caller.Pawn as Pawn;
-		if (pawn.IsValid() && !pawn.Dead)
+		if ( pawn.IsValid() && !pawn.Dead )
 		{
 			pawn.Health = 0;
 			pawn.Die();
 		}
+	}
+
+	[ConCmd.Server( "toggle_upgrade_panel" )]
+	public static void ToggleUpgradePanelCmd()
+	{
+		var pawn = ConsoleSystem.Caller.Pawn as Pawn;
+		if ( pawn.IsValid() && !pawn.Dead )
+		{
+			pawn.IsUpgradePanelOpen = !pawn.IsUpgradePanelOpen;
+		}
+	}
+
+	[ConCmd.Server( "give_upgrade_points" )]
+	public static void GiveUpgradePoints( int points )
+	{
+		var pawn = ConsoleSystem.Caller.Pawn as Pawn;
+		if ( pawn.IsValid() )
+			pawn.UpgradePoints += points;
 	}
 }
