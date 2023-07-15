10bdf32a0229ae14e06f20516f1aee2cf2f80ca7
youarereadingthis <>
2023-07-15 13:26:28 -0400

    Change wave length to be time-based instead of spawnrate dependent

--- code/Game.Waves.cs
+++ code/Game.Waves.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,11 +14,10 @@ public partial class DeadLines : Sandbox.GameManager
 	public int WaveCount { get; set; } = 0;
 	[Net]
 	public TimeUntil NextWave { get; set; } = 0f;
+	public static TimeUntil WaveEnd { get; set; } = 0f;
 	public static bool WaveOver { get; set; }
-	public static float SpawnBank { get; set; } = 100f;
-	public static float SpawnBankMax { get; set; } = 100f;
-	public static float SpawnBankBase { get; set; } = 350f;
-	public static float WaveBurstBankScale { get; set; } = 0.5f;
+	public static float WaveBaseDuration { get; set; } = 60f;
+	public static float WaveCountDuration { get; set; } = 1f;
 
 	// Level of challenge, per wave.
 	public static float IntensityMin { get; set; }
@@ -69,15 +67,15 @@ public partial class DeadLines : Sandbox.GameManager
 				p.HideUpgradeScreen();
 			}
 
-			// Continuously spawn until we're outta juice.
-			// Then wait until the next wave.
+			// Continuously spawn until the wave is over.
+			Log.Info( "WaveFraction() = " + WaveFraction() );
 
-			if ( SpawnBank <= 0f )
+			if ( WaveFraction() >= 1f )
 			{
 				if ( EnemyCount() == 0 )
 					FinishWave();
 			}
-			else if ( NextBurst && SpawnBank > 100f )
+			else if ( NextBurst )
 			{
 				StartBursting();
 			}
@@ -105,7 +103,7 @@ public partial class DeadLines : Sandbox.GameManager
 				else
 				{
 					// How far is the wave along?
-					float skill = MathX.Lerp( IntensityMin, IntensityMax, 1f - (SpawnBank / SpawnBankMax), true );
+					float skill = MathX.Lerp( IntensityMin, IntensityMax, WaveFraction(), true );
 					float frac = MathX.Clamp( skill / IntensityLimit, 0f, 1f );
 					// Log.Info( "skill:" + skill );
 					// Log.Info( "frac:" + frac );
@@ -115,10 +113,18 @@ public partial class DeadLines : Sandbox.GameManager
 					NextSpawn = MathX.Lerp( SpawnDelayMax, SpawnDelayMin, frac, true ) / pCount;
 					// Log.Info( "NextSpawn:" + NextSpawn );
 					// Spawn the enemy and subtract its value from our bank.
-					SpawnBank = MathF.Max( 0f, SpawnBank - SpawnEnemy() );
+					SpawnEnemy();
 				}
 			}
 		}
+	}
+
+	/// <summary>
+	/// A fraction of how far along the wave is.
+	/// </summary>
+	public static float WaveFraction()
+	{
+		return WaveEnd.Fraction;
 	}
 
 	public static void FinishWave()
@@ -163,13 +169,12 @@ public partial class DeadLines : Sandbox.GameManager
 
 		// Spawn more enemies per wave.
 		var pCount = MathF.Max( 1f, PlayerCount() );
-		SpawnBankMax = (SpawnBankBase + (Manager.WaveCount * 70f)) * pCount;
-		SpawnBank = SpawnBankMax;
+		WaveEnd = WaveBaseDuration + (Manager.WaveCount * WaveCountDuration);
 
 		// Log.Info( "IntensityMin:" + IntensityMin );
 		// Log.Info( "IntensityMax:" + IntensityMax );
 
-		DeadLines.Manager.NextWave = delay;
+		Manager.NextWave = delay;
 		NextSpawn = 0f;
 
 		Manager.WaveCount++;
@@ -181,7 +186,7 @@ public partial class DeadLines : Sandbox.GameManager
 		// Log.Info( "Spawning a burst of enemies." );
 
 		// Give the player just a bit of time to react.
-		if ( SpawnBank != SpawnBankMax )
+		if ( WaveFraction() != 0f )
 			NextSpawn = 3f;
 
 		ShouldBurst = true;
@@ -300,7 +305,7 @@ public partial class DeadLines : Sandbox.GameManager
 	{
 		for ( int i = 0; i < 15 * scale; i++ )
 		{
-			SpawnBank = MathF.Max( 0f, SpawnBank - (SpawnTriangle() * WaveBurstBankScale) );
+			SpawnTriangle();
 		}
 	}
 
@@ -308,7 +313,7 @@ public partial class DeadLines : Sandbox.GameManager
 	{
 		for ( int i = 0; i < 10 * scale; i++ )
 		{
-			SpawnBank = MathF.Max( 0f, SpawnBank - (SpawnSquare() * WaveBurstBankScale) );
+			SpawnSquare();
 		}
 	}
 
@@ -316,7 +321,7 @@ public partial class DeadLines : Sandbox.GameManager
 	{
 		for ( int i = 0; i < 7 * scale; i++ )
 		{
-			SpawnBank = MathF.Max( 0f, SpawnBank - (SpawnSnake() * WaveBurstBankScale) );
+			SpawnSnake();
 		}
 	}
 
@@ -324,7 +329,7 @@ public partial class DeadLines : Sandbox.GameManager
 	{
 		for ( int i = 0; i < 12 * scale; i++ )
 		{
-			SpawnBank = MathF.Max( 0f, SpawnBank - (SpawnGate() * WaveBurstBankScale) );
+			SpawnGate();
 		}
 	}
 }
